20a279c2d25ac7e2795cc0b5515732f8fc77ba5a
H4ch1 <jose.ilizarbee@gmail.com>
2024-08-31 11:17:54 -0500

    Config: absolute route +  add primeflex css

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import {provideHttpClient} from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations'
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,6 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), 
     importProvidersFrom(ModalModule.forRoot()),
     provideHttpClient(),
-    
+    provideAnimations(),    
   ]
 };
