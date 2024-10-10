9bc729c52867fff9f9a06951e2df3fdee9fd9caa
alexisc13 <103052010+alexisc13@users.noreply.github.com>
2024-10-10 13:39:09 -0500

    primer reporte

--- src/app/components/cuadro-controles-reportes/reporte-cronico/reporte-cronico.component.ts
+++ src/app/components/cuadro-controles-reportes/reporte-cronico/reporte-cronico.component.ts
@@ -1,12 +1,181 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { forkJoin } from 'rxjs';
+import Swal from 'sweetalert2';
+import moment from 'moment';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HistoriaCuidadoDTO } from '@models/historia-cuidado';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { HistoriaService } from '@services/historia.service';
+import { ControlEpocDTO } from '@models/control-epoc';
+import { DesplegableDTO } from '@models/depleglable';
 
 @Component({
   selector: 'app-reporte-cronico',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+    ReactiveFormsModule,
+    FormsModule],
   templateUrl: './reporte-cronico.component.html',
   styleUrl: './reporte-cronico.component.css'
 })
-export class ReporteCronicoComponent {
+export class ReporteCronicoComponent implements OnInit {
+
+  dataFormGroup: FormGroup;
+  objHistoria=new HistoriaCuidadoDTO();
+  idHistoria:number=0;
+  verSpinner:boolean = false;
+
+  paciente:string ='';
+  medico:string='';
+  nroHcl?:string='';
+
+  objControlEpoc: ControlEpocDTO[]=[];
+
+  constructor(
+    private bsModalReporte: BsModalRef,
+    private historiaService: HistoriaService,
+  ){
+    this.dataFormGroup = new FormGroup({
+
+    });
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  AsignarHistoriaClinica(historia:HistoriaCuidadoDTO, idHistoria:number){
+
+    console.log("historia llega", historia);
+    this.objHistoria = historia;
+    this.idHistoria = idHistoria;
+    //this.objControlPresion = historia.ControlPresion;
+    this.paciente = historia.cabeceraPaciente?.ApellidoPaterno+' '+historia.cabeceraPaciente?.ApellidoMaterno+', '+historia.cabeceraPaciente?.Nombre;
+    this.medico = this.objHistoria.MedicoAtiende?.ApellidoPaterno+' '+this.objHistoria.MedicoAtiende?.ApellidoMaterno+', '+this.objHistoria.MedicoAtiende?.Nombre;
+    this.AsignarObjetoHistoria(historia);
+  }
+
+  AsignarObjetoHistoria(data:any)
+  {
+    this.verSpinner = true;
+    let objHistoria: any = data;
+    console.log("objHistoria",objHistoria);
+  
+    this.nroHcl = this.objHistoria.cabeceraPaciente?.NumeroDocumento;
+
+    let controlEpoc : ControlEpocDTO[] = [];
+    if(objHistoria.controlEpoc != null){
+      objHistoria.controlEpoc.forEach((element:any) =>{
+
+        let epoc = new ControlEpocDTO();
+        epoc.Paciente= element.paciente;
+        epoc.FechaRegistro= element.fechaRegistro;
+        epoc.Banno= new DesplegableDTO();
+        epoc.Banno.Id = element.banno.id;
+        epoc.Banno.Nombre = element.banno.nombre;
+        epoc.Vestido= new DesplegableDTO();
+        epoc.Vestido.Id = element.vestido.id;
+        epoc.Vestido.Nombre = element.vestido.nombre;
+        epoc.Wc= new DesplegableDTO();
+        epoc.Wc.Id = element.wc.id;
+        epoc.Wc.Nombre = element.wc.nombre;
+        epoc.Movilidad= new DesplegableDTO();
+        epoc.Movilidad.Id = element.movilidad.id;
+        epoc.Movilidad.Nombre = element.movilidad.nombre;
+        epoc.Continencia= new DesplegableDTO();
+        epoc.Continencia.Id = element.continencia.id;
+        epoc.Continencia.Nombre = element.continencia.nombre;
+        epoc.Alimentacion= new DesplegableDTO();
+        epoc.Alimentacion.Id = element.alimentacion.id;
+        epoc.Alimentacion.Nombre = element.alimentacion.nombre;
+        epoc.ResultadoEscala= new DesplegableDTO();
+        epoc.ResultadoEscala.Id = element.resultadoEscala.id;
+        epoc.ResultadoEscala.Nombre = element.resultadoEscala.nombre;
+        epoc.Dificultad= element.dificultad;
+        epoc.FaseEpoc= new DesplegableDTO();
+        epoc.FaseEpoc.Id = element.faseEpoc.id;
+        epoc.FaseEpoc.Nombre = element.faseEpoc.nombre;
+        epoc.FechaDiagnostico= element.fechaDiagnostico;
+        epoc.Alcohol= new DesplegableDTO();
+        epoc.Alcohol.Id = element.alcohol.id;
+        epoc.Alcohol.Nombre = element.alcohol.nombre;
+        epoc.Drogas= new DesplegableDTO();
+        epoc.Drogas.Id = element.drogas.id;
+        epoc.Drogas.Nombre = element.drogas.nombre;
+        epoc.Tabaco= new DesplegableDTO();
+        epoc.Tabaco.Id = element.tabaco.id;
+        epoc.Tabaco.Nombre = element.tabaco.nombre;
+        epoc.SistemaRespiratorio= new DesplegableDTO();
+        epoc.SistemaRespiratorio.Id = element.sistemaRespiratorio.id;
+        epoc.SistemaRespiratorio.Nombre = element.sistemaRespiratorio.nombre;
+        epoc.SistemaRespiratorioDetalle= element.sistemaRespiratorioDetalle;
+        epoc.EvaluacionFuncional= element.evaluacionFuncional;
+        epoc.PlanTrabajo= element.planTrabajo;
+
+        controlEpoc.push(epoc);
+      });
+    
+    }
+  }
+
+  
+  MostrarNotificacionInfo(mensaje: string, titulo: string) {
+    Swal.fire({
+      icon: 'info',
+      title: titulo,
+      text: mensaje
+    });
+  }
+
+  MostrarNotificacionWarning(mensaje: string, titulo: string) {
+    Swal.fire({
+      icon: 'warning',
+      title: titulo,
+      text: mensaje
+    });
+  }
+  manejadorMensajeErroresGuardar(e: any) {
+    if (typeof e != "string") {
+      let error = e;
+      let arrayErrores: any[] = [];
+      let errorValidacion = Object.keys(e);
+      if (Array.isArray(errorValidacion)) {
+        errorValidacion.forEach((propiedadConError: any) => {
+          error[propiedadConError].forEach((mensajeError: any) => {
+            if (!arrayErrores.includes(mensajeError['mensaje'])) {
+              arrayErrores.push(mensajeError['mensaje']);
+            }
+          });
+        });
+        this.MostrarNotificacionError(arrayErrores.join("<br/>"), '¡ERROR EN EL PROCESO!')
+      } else {
+        this.MostrarNotificacionError("", '¡ERROR EN EL PROCESO!')
+      }
+    }
+    else {
+      this.MostrarNotificacionError(e, '¡ERROR EN EL PROCESO!');
+    }
+  }
+  MostrarNotificacionError(mensaje: string, titulo: string) {
+    Swal.fire({
+      icon: 'error',
+      title: titulo,
+      html: `<div class="message-text-error">${mensaje} </div>`,
+    });
+  }
+
+  MostrarNotificacionSuccessModal(mensaje: string, titulo: string) {
+    Swal.fire({
+      icon: 'success',
+      title: titulo,
+      text: mensaje
+    });
+  }
+
+  CerrarModal() {
+    this.bsModalReporte.hide();
+    //this.onGuardar();
+  }
 
 }
