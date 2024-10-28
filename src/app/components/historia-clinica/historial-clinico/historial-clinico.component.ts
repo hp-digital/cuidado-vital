5bccffc29a8f0a1b509a90b5bb9998f0128b3792
alexisc13 <103052010+alexisc13@users.noreply.github.com>
2024-10-27 20:46:23 -0500

    repositorio historial

---
+++ src/app/components/historia-clinica/historial-clinico/historial-clinico.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historial-clinico',
+  standalone: true,
+  imports: [],
+  templateUrl: './historial-clinico.component.html',
+  styleUrl: './historial-clinico.component.css'
+})
+export class HistorialClinicoComponent {
+
+}
